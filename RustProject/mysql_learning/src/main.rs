dbe9c32a4ae9d158fc056b1d3e6899ff77847fc1
DESKTOP-20QCJGK\Administrator <964648935@qq.com>
2024-08-15 23:24:59 +0800

    chore(*): 读取db文件学习。

--- RustProject/mysql_learning/src/main.rs
+++ RustProject/mysql_learning/src/main.rs
@@ -1,16 +1,45 @@
+mod db_config;
+
 use mysql::prelude::*;
 use mysql::*;
+use std::fs::File;
+use std::io::prelude::*;
+use serde_json;
+use crate::db_config::*; // 导入结构体
 
-fn main() {
-    let dsn: &str = "";
-    let pool: Pool = Pool::new(dsn).unwrap();
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    let mut file = File::open("db/config/db_connection.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let config: DbConfig = serde_json::from_str(&contents)?;
+
+    let db_config = &config.毛古哥服务器;
+    // println!("Host: {}", db_config.host);
+    // println!("Port: {}", db_config.port);
+    // println!("User: {}", db_config.user);
+    // println!("Password: {}", db_config.password);
+
+    let dsn= format!(
+        "mysql://{}:{}@{}:{}/{}",
+        db_config.user,
+        db_config.password,
+        db_config.host,
+        db_config.port,
+        db_config.database
+    );
+    let dsn_slice: &str = dsn.as_str();
+    let pool: Pool = Pool::new(dsn_slice).unwrap();
     let mut conn = pool.get_conn().unwrap();
     let ret: Option<String> = conn
         .query_first("select f1 from log_pull;")
         .unwrap();
     println!("{}", ret.unwrap());
 
-    println!("{}", gcd(14, 100))
+    println!("{}", gcd(14, 100));
+
+    Ok(())
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
