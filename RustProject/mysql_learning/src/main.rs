317edc7d5787a4e54e7f85e981061ebd66ff8179
DESKTOP-20QCJGK\Administrator <964648935@qq.com>
2024-08-15 22:14:37 +0800

    chore(*): 提交第一次学习代码。

---
+++ RustProject/mysql_learning/src/main.rs
@@ -0,0 +1,35 @@
+use mysql::prelude::*;
+use mysql::*;
+
+fn main() {
+    let dsn: &str = "mysql://tests:fNsKpFTFhC4TWJ5P@43.229.212.82:3306/tests";
+    let pool: Pool = Pool::new(dsn).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let ret: Option<String> = conn
+        .query_first("select f1 from log_pull;")
+        .unwrap();
+    println!("{}", ret.unwrap());
+
+    println!("{}", gcd(14, 100))
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
+
